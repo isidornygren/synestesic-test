7cfd5711662f7991d6dba28073d3200aa0fb735e
isidornygren <karlisidornygren@gmail.com>
2017-10-05 11:01:58 +0200

    Styling

--- server.js
+++ server.js
@@ -3,7 +3,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
+
 var port = 8080;
+
+//Set up default mongoose connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/testing_db');
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
